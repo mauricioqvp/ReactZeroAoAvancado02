4c46ac071f492325143e98fe3925bc6a80b4c852
Maurício Quaranta <mauricioqvp@yahoo.com.br>
2024-03-11 23:01:36 -0300

    Api UseState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, {useState} from "react";
 
 function App() {
+
+const [tarefas, setTarefas] = useState([ 
+  'Estudar Cálculo',
+  'Fazer lista de apartamentos'
+]);
+
+const [input, setInput] = useState('');
+
+function handleAdd(){
+  setTarefas([...tarefas, input]);
+  setInput('');
+}
+
   return (
     <div>
-      <h1>Hooks</h1>
+      <h1>Tarefas</h1>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <input type="text" name="entrada" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Cadastrar</button>
     </div>
   );
 }
